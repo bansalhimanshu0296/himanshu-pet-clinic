57598b131880939e312af03d36bc70744c473e12
bansalhimanshu0296 <bansalhimanshu0296@gmail.com>
2020-04-04 16:13:14 +0530

    Initial Project Add

---
+++ src/main/java/himanshu/springframework/himanshupetclinic/HimanshuPetClinicApplication.java
@@ -0,0 +1,13 @@
+package himanshu.springframework.himanshupetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HimanshuPetClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HimanshuPetClinicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/himanshu/springframework/himanshupetclinic/HimanshuPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package himanshu.springframework.himanshupetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HimanshuPetClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
