8ac3ca4f9c14ce4d35e7fba9a082c454d0d3d164
bansalhimanshu0296 <bansalhimanshu0296@gmail.com>
2020-04-14 10:12:31 +0530

    Owners Index Page and Controller added. Closes #13.

--- pet-clinic-web/src/main/java/himanshu/springframework/himanshupetclinic/controllers/OwnersController.java
+++ pet-clinic-web/src/main/java/himanshu/springframework/himanshupetclinic/controllers/OwnersController.java
@@ -3,12 +3,12 @@ package himanshu.springframework.himanshupetclinic.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@RequestMapping("/vets")
+@RequestMapping("/owners")
 @Controller
-public class VetsController {
+public class OwnersController {
 
     @RequestMapping({"","/","/index","/index.html"})
     public String listVets(){
-        return "vets/index";
+        return "owners/index";
     }
 }
