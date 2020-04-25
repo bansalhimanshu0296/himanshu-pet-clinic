c6111d2b3db1c3a56f3ae2b9dacf0006e137846a
bansalhimanshu0296 <bansalhimanshu0296@gmail.com>
2020-04-26 02:58:21 +0530

    Visit Map Service Implemented. closes #46

---
+++ pet-clinic-data/src/main/java/himanshu/springframework/himanshupetclinic/services/VisitService.java
@@ -0,0 +1,6 @@
+package himanshu.springframework.himanshupetclinic.services;
+
+import himanshu.springframework.himanshupetclinic.model.Visit;
+
+public interface VisitService extends CrudService<Visit,Long> {
+}
---
+++ pet-clinic-data/src/main/java/himanshu/springframework/himanshupetclinic/services/map/VisitServiceMapImpl.java
@@ -0,0 +1,15 @@
+package himanshu.springframework.himanshupetclinic.services.map;
+
+import himanshu.springframework.himanshupetclinic.model.Visit;
+import himanshu.springframework.himanshupetclinic.services.VisitService;
+
+public class VisitServiceMapImpl extends AbstractMapService<Visit,Long> implements VisitService {
+    @Override
+    public Visit save(Visit object) {
+        if(object.getPet() == null || object.getPet().getId() == null || object.getPet().getOwner() == null ||
+        object.getPet().getOwner().getId() == null){
+            throw new RuntimeException("Invalid Visit");
+        }
+        return super.save(object);
+    }
+}
